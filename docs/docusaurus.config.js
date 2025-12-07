ef368ab8b69ac03a35f238c4f3ec2537adad2d38
codewithzubi <159233893+codewithzubi@users.noreply.github.com>
2025-12-07 18:58:36 +0500

    Done

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -20,10 +20,10 @@ const config = {
   },
 
   // Set the production url of your site here
-  url: 'https://codewithzubi.github.io',
+  url: 'https://physical-ai-humanoid-robotic-book-3aan.vercel.app',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/Physical-AI-Humanoid-Robotic-Book/',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
